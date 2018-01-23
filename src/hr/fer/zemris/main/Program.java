db54edbd5701b72947b51a4897b067ad83861ece
Dario Sindicic <dario.sindicic@gmail.com>
2018-01-23 21:58:19 +0100

    preparing to build up the testing structure

--- src/hr/fer/zemris/main/Program.java
+++ src/hr/fer/zemris/main/Program.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import javax.swing.SwingUtilities;
 
@@ -33,6 +34,11 @@ public class Program
 	private final double minValue = -180.0;
 	private final double maxValue = 180.0;
 	private final int numOfBuckets = 7000000; 
+	
+	private final double CHANGE_RATE = 0.5;
+	private final int ITERATIONS = 5;
+	
+	private Random rand = new Random();
 	
 	private Structure[] S;
 	
@@ -62,12 +68,23 @@ public class Program
 		for(int i=0, len=dots.size(); i<len; ++i)
 		{
 			double[] value = dots.get(i).getValues();
-			
 			for(int j=0, len2 = value.length; j<len2; ++j)
 				S[i].add(value[j], dots.get(i).getId());
 		}
 		
+		int iter = ITERATIONS;
+		while(iter-->=0)
+		{
+			double randValue = rand.nextDouble();
+			if(randValue<changeFactor)
+				updatePosition();
+			randValue = rand.nextDouble();
+			if(randValue<queryFactor)
+				runQuery(1);
+		}
+		
 	}
+	
 	private void initStructure() throws InvalidStructureType {
 		
 		if(structureType==1)
@@ -99,6 +116,33 @@ public class Program
 			}
 			Dot dot = new Dot(numOfArguments , numOfLine-1);
 			dots.add(dot);
+		}
+	}
+	private void updatePosition()
+	{
+		for(int i=0, len=dots.size(); i<len; ++i)
+		{
+			for(int j=0; j<numOfArguments; ++j)
+			{
+				double newValue = dots.get(i).getValue(j) + CHANGE_RATE * rand.nextGaussian();
+				S[j].update(dots.get(i).getValue(j), newValue, dots.get(i).getId());
+			}
+		}
+	}
+	private void runQuery(int num)
+	{
+		double diff = Math.abs(maxValue-minValue - 50);
+		while(num-->0)
+		{
+			List<Integer>[] result = new ArrayList[numOfArguments];
+			for(int i=0;i<numOfArguments;++i)
+			{
+				double min = rand.nextDouble()*diff + minValue;
+				double max = min + 15;
+				result[i] = new ArrayList<>();
+				result[i] = S[i].query(min, max);
+			}
+			//todo intersection
 		}
 	}
 }
